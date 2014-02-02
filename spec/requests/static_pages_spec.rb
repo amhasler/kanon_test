4fcb32c062da3ceebe40f130e6c97ef7313fb9fc
a.m. hasler <amhasler@gmail.com>
2014-02-02 09:38:00 -0500

    Basic user model

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,32 +2,47 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-	let(:base_title) { "Project Page One |" }
-
   subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
 
   describe "Home page" do
     before { visit root_path }
+    let(:heading)    { 'Project Page One' }
+    let(:page_title) { '' }
 
-    it { should have_content('Project Page One') }
-    it { should have_title(full_title('')) }
-    it { should_not have_title('| Home') }
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
 
     before { visit '/about' }
 
-  	it { should have_content('About the project') }
-    it { should have_title(full_title('About')) }
+  	let(:heading)    { 'About' }
+    let(:page_title) { 'About' }
 	end
 
 	describe "Contact page" do
 
     before { visit '/contact' }
 
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
+    let(:heading)    { 'Contact' }
+    let(:page_title) { 'Contact' }
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Sign up"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "PP1"
+    expect(page).to have_title(full_title(''))
   end
 
 end
